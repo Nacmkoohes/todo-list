0d0b42e560d0b2f5742e7be34fbaf2cce5ed0e25
Nasim Koohestani <nasimkoohestani82@gamil.com>
2025-10-11 14:40:05 +0330

    feat: add Task class to manage tasks

--- main.py
+++ main.py
@@ -9,3 +9,16 @@ class Project:
         self.tasks.remove(task)
     def __str__(self):
         return f"Project Name: {self.name} Tasks: {self.tasks} Description:{str(self.description)}"
+class Task:
+    def __init__(self,title,description,deadline,status="Todo"):
+        self.title = title
+        self.description = description
+        self.deadline = deadline
+        self.status = status
+    def change_status(self, new_status):
+        if new_status in ["Todo","Doing","Done"]:
+            self.status = new_status
+        else:
+            print("Invalid status")
+    def __str__(self):
+        return f"Task Title: {self.title}, Status: {self.status}, Deadline: {self.deadline}"
